8162ed4b29f4e240694b7296be75f1c28a40a49f
Liudmila <lohmataja@gmail.com>
2014-08-19 14:33:15 -0400

    Pretty printer for Python ASTs.

---
+++ ast_pretty_printer.py
@@ -0,0 +1,194 @@
+import ast, inspect
+from functools import wraps
+
+class DecorativePrinter(ast.NodeVisitor):
+    def __init__(self):
+        self.res = ''
+        self.prefix = ''
+
+    def gen_visit_deco(f):
+        @wraps(f)
+        def new_visit(*args, **kwargs):
+            self = args[0]
+            self.prefix += '\t'
+            f(*args, **kwargs)
+            self.prefix = self.prefix[:-1]
+        return new_visit
+
+    @gen_visit_deco
+    def generic_visit(self, node):
+        print(self.prefix+type(node).__name__)
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_Module(self, node):
+        print(type(node).__name__)
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_FunctionDef(self, node):
+        print(self.prefix + type(node).__name__ +': '+ node.name)
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_arguments(self, node):
+        print(self.prefix + type(node).__name__ +': ')
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_Name(self, node):
+        print(self.prefix+'Name:' + node.id)
+
+    def visit_Num(self, node):
+        print(self.prefix+'Num:' + str(node.__dict__['n']))
+
+    def visit_Str(self, node):
+        print(self.prefix+'Str:' + node.s)
+
+    def visit_Print(self, node):
+        print(self.prefix+'Print:')
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_Assign(self, node):
+        print(self.prefix+'Assign:')
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_Expr(self, node):
+        print(self.prefix+'Expr:')
+        ast.NodeVisitor.generic_visit(self, node)
+
+class PrettyPrinter(ast.NodeVisitor):
+    def __init__(self):
+        self.res = ''
+        self.begin = '&&&'
+        self.end = '%%%'
+        self.prefix = ''
+
+    def generic_visit(self, node):
+        print(self.prefix+type(node).__name__)
+        self.prefix += '\t'
+        ast.NodeVisitor.generic_visit(self, node)
+        self.prefix = self.prefix[:-1]
+
+    def visit_FunctionDef(self, node):
+        print(self.prefix + type(node).__name__ +': '+ node.name)
+        self.prefix += '\t'
+        ast.NodeVisitor.generic_visit(self, node)
+        self.prefix = self.prefix[:-1]
+
+    def visit_arguments(self, node):
+        print(self.prefix + type(node).__name__ +': ')
+        self.prefix += '\t'
+        ast.NodeVisitor.generic_visit(self, node)
+        self.prefix = self.prefix[:-1]
+
+    def visit_Name(self, node):
+        print(self.prefix+'Name:' + node.id)
+
+    def visit_Num(self, node):
+        print(self.prefix+'Num:' + str(node.__dict__['n']))
+
+    def visit_Str(self, node):
+        print(self.prefix+'Str:' + node.s)
+
+    def visit_Print(self, node):
+        print(self.prefix+'Print:')
+        self.prefix += '\t'
+        ast.NodeVisitor.generic_visit(self, node)
+        self.prefix += '\t'
+
+    def visit_Assign(self, node):
+        print(self.prefix+'Assign:')
+        self.prefix += '\t'
+        ast.NodeVisitor.generic_visit(self, node)
+        self.prefix += '\t'
+
+    def visit_Expr(self, node):
+        print(self.prefix+'Expr:')
+        self.prefix += '\t'
+        ast.NodeVisitor.generic_visit(self, node)
+        self.prefix += '\t'
+
+    def pretty_print(self):
+        b = len(self.begin)
+        e = len(self.end)
+
+        def rec_print(string, level=0):
+            if not self.res.startswith(self.begin) or len(string) <= (b + e):
+                print('\t' * level + string)
+            else:
+                i = string.find(self.begin, len(self.begin))
+                print('\t' * level + string[len(self.begin):i])
+                rec_print(string[i:len(string) - len(self.end)], level + 1)
+
+        assert self.res.startswith(self.begin) and self.res.endswith(self.end)
+        rec_print(self.res)
+
+
+class UglyPrinter(ast.NodeVisitor):
+    def __init__(self):
+        self.res = []
+
+    def generic_visit(self, node):
+        res = []
+        res.append(type(node).__name__)
+        res.append(ast.NodeVisitor.generic_visit(self, node))
+        return res
+
+    def visit_Name(self, node):
+        res = []
+        res.append('Name:' + node.id)
+        return res
+
+    def visit_Num(self, node):
+        res = []
+        res.append('Num:' + str(node.__dict__['n']))
+        return res
+
+    def visit_Str(self, node):
+        res = []
+        res.append('Str:' + node.s)
+        return res
+
+    def visit_Print(self, node):
+        res = []
+        res.append('Print:')
+        res.append(ast.NodeVisitor.generic_visit(self, node))
+        return res
+
+    def visit_Assign(self, node):
+        res = []
+        res.append('Assign:')
+        res.append(ast.NodeVisitor.generic_visit(self, node))
+        return res
+
+    def visit_Expr(self, node):
+        res = []
+        res.append('Expr:')
+        res.append(ast.NodeVisitor.generic_visit(self, node))
+        return res
+
+    def pretty_print(self, s):
+        def rec_print(s, level=0):
+            if type(s) == list:
+                for item in s:
+                    rec_print(item, level + 1)
+            else:
+                print('\t' * level + str(s))
+
+        rec_print(s)
+
+
+# def f(n, accum=1):
+#     if n <= 1:
+#         return accum
+#     else:
+#         return f(n - 1, accum * n)
+#
+#
+# tree = ast.parse(inspect.getsource(f))
+tree = ast.parse(open('./test.py', 'r').read())
+print(ast.dump(tree))
+v = PrettyPrinter()
+v.visit(tree)
+# v.pretty_print()
+
+# u = UglyPrinter()
+# res = u.visit(tree)
+# u.pretty_print(res)
