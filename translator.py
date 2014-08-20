d905895b931979ab904d5f6d47ab9ac0846cef76
Liudmila <lohmataja@gmail.com>
2014-08-20 14:55:38 -0400

    Added RecursionTransformer. Implemented adding arguments and default values to the function's signature.

--- translator.py
+++ translator.py
@@ -1,5 +1,6 @@
 __author__ = 'Liuda'
 import ast, inspect
+from ast_pretty_printer import polish
 
 def isRecursive(tree):
     try:
@@ -17,4 +18,39 @@ def isRecursive(tree):
         return False
 
 class RecursionTranslator(ast.NodeTransformer):
+    default_value = 1
+
+    def make_Name(self, name):
+        return ast.Name(
+            id=name,
+            ctx=ast.Param()
+        )
+
+    def make_Num(self, x):
+        return ast.Num(n=x)
+
     def visit_FunctionDef(self, node):
+        node.args.args.append(self.make_Name('accum'))
+        node.args.defaults.append(self.make_Num(self.default_value))
+        print(node.args.args)
+        print(node.args.defaults)
+        return node
+
+
+def tail_fact(n, accum=1):
+    if n <= 1:
+        return accum
+    else:
+        return tail_fact(n - 1, accum * n)
+
+def rec_fact(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * rec_fact(n-1)
+
+rt = RecursionTranslator()
+tree = ast.parse(inspect.getsource(rec_fact))
+rt.visit(tree)
+print(ast.dump(tree))
+print(polish(ast.dump(tree)))
